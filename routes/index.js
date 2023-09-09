a5803cfcf7611b0d99753bbbae26f63d2cbb03de
Sergey Dobrovolskiy <SFrontEnd@yandex.ru>
2023-09-09 23:02:49 +0300

    Сдача проекта Mesto: подготовка бэкенда

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,7 @@ const router = Router();
 
 router.use('/users', usersRouter);
 router.use('/cards', cardsRouter);
+
 router.use((req, res) => {
   res.status(404).send({ message: 'Ошибка 404. Страница не найдена' });
 });
