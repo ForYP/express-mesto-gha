b976a1053fac61aa77e839f95099382df010f315
Sergey Dobrovolskiy <SFrontEnd@yandex.ru>
2023-09-09 22:32:12 +0300

    Сдача проекта Mesto: подготовка бэкенда

--- app.js
+++ app.js
@@ -10,8 +10,6 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect(DB_URL);
-
 app.use((req, res, next) => {
   req.user = {
     _id: '64fa07d01d0676b353deb9a6', // вставьте сюда _id созданного в предыдущем пункте пользователя
@@ -21,6 +19,7 @@ app.use((req, res, next) => {
 });
 
 app.use(router);
+mongoose.connect(DB_URL);
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
 });
