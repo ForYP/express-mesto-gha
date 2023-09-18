852e92dd8ca5455f87ea322eed058565dcc5b7ab
Sergey Dobrovolskiy <SFrontEnd@yandex.ru>
2023-09-18 21:56:05 +0300

    Сдача проекта Mesto: регистрация и авторизация на бэкенде

--- routes/index.js
+++ routes/index.js
@@ -11,4 +11,8 @@ router.use(auth);
 router.use('/users', usersRouter);
 router.use('/cards', cardsRouter);
 
+router.use('*', (req, res) => {
+  res.status(404).send({ message: 'Ошибка 404. Страница не найдена' });
+});
+
 module.exports = router;
