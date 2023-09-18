7a58ed21ffc6bf9bc642cb116405cba84a0fca1f
Sergey Dobrovolskiy <SFrontEnd@yandex.ru>
2023-09-18 20:55:27 +0300

    Сдача проекта Mesto: регистрация и авторизация на бэкенде

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const router = require('./routes');
 const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 
 const app = express();
-app.use(helmet());
+// app.use(helmet());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
